54cddc9e92ec102528732871f360a0842feb2980
Kembolle Amilkar <contato@kembolle.com.br>
2017-08-22 04:17:30 -0400

    .

--- shadow/box/linux/network/search-mac.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-arp-scan --interface=enp63s0 172.20.20.0/22 > /home/oliverkall/desktop/MAC-Clonados.txt
--- shadow/box/linux/network/search-ssdp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gssdp-discover -i enp3s0 --timeout=3 > /home/oliverkall/desktop/ssdp.txt
-
-
-
