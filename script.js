ac0bfd23f915cf4d4b20b7db45cd5f915a11a801
kembolle <contato@kembolle.com.br>
2025-04-08 12:15:02 -0400

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+navigator.geolocation.getCurrentPosition((pos) => {
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+
+    // Salva no localStorage
+    localStorage.setItem('gps_lat', latitude);
+    localStorage.setItem('gps_lng', longitude);
+
+    console.log("Localização salva com sucesso!");
+}, (err) => {
+    console.error("Erro ao obter localização:", err);
+});
+</script>
